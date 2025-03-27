cdaeab411d7b3666ab061fb658a02505525a294e
cs4151 <hannah.gabele@student.uibk.ac.at>
2025-03-27 15:35:57 +0100

    KOmmentare eingefügt

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+/*
+Skript für Neuseelandreise
+
+*/
+
+// Karte initialisieren
+
 var map = L.map('map').setView([47.66336, 9.17598], 14);
+
+// Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Marker zeichnen
 var marker = L.marker([47.66336, 9.17598]).addTo(map);
+
+// Popup definieren und öffnen
 marker.bindPopup("<b>Hallo Konstanz!</b><br>I am a popup.").openPopup();
