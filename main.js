6d1ecc83513ba8686cfb86e13339d94360e59245
cs4151 <hannah.gabele@student.uibk.ac.at>
2025-03-27 16:10:41 +0100

    Popup geupdated

--- main.js
+++ main.js
@@ -21,5 +21,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let marker = L.marker([lat, lng]).addTo(map);
 
 // Popup definieren und öffnen
-marker.bindPopup(`<b>Hallo Konstanz!</b><br>I am a popup at ${lat.toFixed(5)} / ${lng.toFixed(5)}.
+
+marker.bindPopup(`<h2>Konstanz</h2>
+    <ul>
+    <li>Geogr. Breite: ${lat.toFixed(5)}°</li>
+    <li>Geogr. Länge: ${lng.toFixed(5)}°</li>
+    </ul>
     `).openPopup();
