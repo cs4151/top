9ffbd9c059812f5e39b0ac1261720259a95d303b
cs4151 <hannah.gabele@student.uibk.ac.at>
2025-03-27 15:50:03 +0100

    varaiblene rstellt, Zahlen ersetzt

--- main.js
+++ main.js
@@ -2,19 +2,23 @@
 Skript für Neuseelandreise
 
 */
+let lat = 47.66336;
+let lng = 9.17598;
+let zoom = 19;
 
 // Karte initialisieren
 
 let map = L.map('map').setView([47.66336, 9.17598], 14);
 
+
 // Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: zoom,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 // Marker zeichnen
-let marker = L.marker([47.66336, 9.17598]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 
 // Popup definieren und öffnen
 marker.bindPopup("<b>Hallo Konstanz!</b><br>I am a popup.").openPopup();
