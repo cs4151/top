f12174c7c5978e2dd5132dd8f7a09b16e274cbb5
cs4151 <hannah.gabele@student.uibk.ac.at>
2025-03-27 16:01:10 +0100

    5 KOmmastellen für Koordinaten in popup festgeleget

--- main.js
+++ main.js
@@ -21,5 +21,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let marker = L.marker([lat, lng]).addTo(map);
 
 // Popup definieren und öffnen
-marker.bindPopup(`<b>Hallo Konstanz!</b><br>I am a popup at ${lat} / ${lng}.
+marker.bindPopup(`<b>Hallo Konstanz!</b><br>I am a popup at ${lat.toFixed(5)} / ${lng.toFixed(5)}.
     `).openPopup();
